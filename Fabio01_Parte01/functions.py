60e587d1ac6d5bb8d0fa0f912ac3aae12eba74cc
guilherme daniel de sousa dias <gui.dani13@gmail.com>
2020-03-19 20:41:23 -0300

    adicionado arquivo de módulos

---
+++ Fabio01_Parte01/functions.py
@@ -0,0 +1,10 @@
+def soma2(a,b):
+    return a + b
+
+
+def subtrai2(a,b):
+    return a - b
+
+
+def divide2(a,b):
+    return a / b
--- Fabio01_Parte01/q08.py
+++ Fabio01_Parte01/q08.py
@@ -1,3 +1,4 @@
+import functions
 # 8 - Leia 2 números, calcule e escreva a divisão da soma pela subtração dos números lidos.
 #
 
@@ -5,17 +6,8 @@ def main():
     num1 = int(input("Número 1: "))
     num2 = int(input("Número 2: "))
 
-    resultado = divide2(soma2(num1, num2), subtrai2(num1, num2))
+    resultado = divide2(functions.soma2(num1, num2), functions.subtrai2(num1, num2))
     print(f'Resposta: {resultado}')
-
-def soma2(a,b):
-    return a + b
-
-def subtrai2(a,b):
-    return a - b
-
-def divide2(a,b):
-    return a / b
 
 
 main()
