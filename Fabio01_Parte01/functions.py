71dbb705bcd6822f51c5788721021d09ba5fea60
guilherme daniel de sousa dias <gui.dani13@gmail.com>
2020-03-19 21:18:44 -0300

    mais atualizacoes

--- Fabio01_Parte01/functions.py
+++ Fabio01_Parte01/functions.py
@@ -8,3 +8,27 @@ def subtrai2(a,b):
 
 def divide2(a,b):
     return a / b
+
+
+def resto_da_divisao(a,b):
+    return a % b
+
+def acha_centena(n):
+    '''
+    Função válida para números de 3 dígitos
+    '''
+    return n // 100
+
+
+def acha_dezena(n):
+    '''
+    Função válida para números de 3 dígitos
+    '''
+    return (n % 100) // 10
+
+
+def acha_unidade(n):
+    '''
+    Função válida para números de 3 dígitos
+    '''
+    return (n % 100) % 10
--- Fabio01_Parte01/q11.py
+++ Fabio01_Parte01/q11.py
@@ -1,18 +1,18 @@
+import functions
 # 11 - Leia um número inteiro (3 dígitos) e escreva o inverso do número. (Ex.: número = 532 ; inverso = 235)
-numero = int(input("Número: "))
 
-# Quebrar o número em unidade, dezenas e centenas
-centena = numero // 100
-dezenas = (numero % 100) // 10
-unidade = (numero % 100) % 10
+def main():
+    numero = int(input("Número: "))
 
-# converter para string
-centena = str(centena)
-dezenas = str(dezenas)
-unidade = str(unidade)
+    centena = str(functions.acha_centena(numero))
+    dezenas = str(functions.acha_dezena(numero))
+    unidade = str(functions.acha_unidade(numero))
 
-# concatenar tudo
-inverso = unidade + dezenas + centena
+    # concatenar tudo
+    inverso = unidade + dezenas + centena
 
-# resultado
-print(f'{numero} ao contrário é {inverso}')
+    # resultado
+    print(f'{numero} ao contrário é {inverso}')
+
+
+main()
