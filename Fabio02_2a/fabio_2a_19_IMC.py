f1140cd79f94e1516da94b70ee25f40bb90adcd4
guilherme daniel de sousa dias <gui.dani13@gmail.com>
2020-03-22 12:29:09 -0300

    stufff

--- Fabio02_2a/fabio_2a_19_IMC.py
+++ Fabio02_2a/fabio_2a_19_IMC.py
@@ -5,16 +5,8 @@ def main():
     altura = float(input("Altura: "))
     peso = float(input("Peso: "))
 
-    #imc = functions.divide2(peso, (altura**2)) # usar função divide2 do módulo functions
     imc = functions.calculo_imc(altura, peso)
-    print(f"IMC: {imc}")
+    # print(f"IMC: {imc}")
 
-'''
-    if imc < 25:
-        print("peso normal")
-    elif imc >= 25 and imc < 30:
-        print("obeso")
-    else:
-        print("Obesidade morbida")
-'''
+
 main()
