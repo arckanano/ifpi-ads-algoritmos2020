493646052ef86b3ba8327efc0363ee924e15066e
guilherme daniel de sousa dias <gui.dani13@gmail.com>
2020-03-25 17:46:37 -0300

    2b16 more changes

--- Fabio02_2b/fabio_2b_16.py
+++ Fabio02_2b/fabio_2b_16.py
@@ -11,34 +11,40 @@ def main():
     
     total_da_compra = 0.0
     if tipo_de_carne == 1:
+        carne = 'File'
         if quantidade_de_carne <= 5:
             total_da_compra = quantidade_de_carne * 4.90
         else:
             total_da_compra = quantidade_de_carne * 5.80
     elif tipo_de_carne == 2:
+        carne = 'Alcatra'
         if quantidade_de_carne <= 5:
             total_da_compra = quantidade_de_carne * 5.90
         else:
             total_da_compra = quantidade_de_carne * 6.80    
     elif tipo_de_carne == 3:
+        carne = 'Picanha'
         if quantidade_de_carne <= 5:
             total_da_compra = quantidade_de_carne * 6.90
         else:
             total_da_compra = quantidade_de_carne * 7.80
 
     desconto = 0.0
-    if modo_de_pagamento == 's'
+    meio_de_pagamento = 'Dinheiro'
+    if modo_de_pagamento == 's':
+        meio_de_pagamento = 'Cartão'
         desconto = total_da_compra * 0.05
 
     valor_total_da_compra = total_da_compra - desconto
 
-    print("--- CUPOM FISCAL ---")
-    print(f'Tipo de carne: {tipo_de_carne}')
-    print(f'Quantidade_de_carne: {quantidade_de_carne}')
-    print(f'Modo de pagamento: {modo_de_pagamento}')
+    print("\n--- CUPOM FISCAL ---")
+    print(f'Tipo de carne: {carne}')
+    print(f'Quantidade_de_carne: {quantidade_de_carne} Kg')
+    print(f'Modo de pagamento: {meio_de_pagamento}')
+    print(f'Valor da compra (s/desc): {total_da_compra}')
     print(f'Desconto = {desconto}')
     print('#####################')
-    print(f'Valor Total: {valor_total_da_compra}')
+    print(f'Valor Total: {valor_total_da_compra:.2f}')
 
 
 main()
