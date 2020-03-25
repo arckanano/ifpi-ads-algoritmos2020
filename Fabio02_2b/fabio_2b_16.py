587af15b331928e930fe2184cbfeaa8467ac792e
guilherme daniel de sousa dias <gui.dani13@gmail.com>
2020-03-25 17:19:37 -0300

    2b16

--- Fabio02_2b/fabio_2b_16.py
+++ Fabio02_2b/fabio_2b_16.py
@@ -1,14 +1,15 @@
 def main():
     menu = print(''''
                 Ate 5Kg     Acima de 5Kg
-    1 - File    R$4.90      R$5.80
-    2 - Alcatra R$5.90      R$6.80
-    3 - Picanha R$6,90      R$7.80
+    1 - file    R$4.90      R$5.80
+    2 - alcatra R$5.90      R$6.80
+    3 - picanha R$6,90      R$7.80
     ''')
-    tipo_de_carne = int(input("Qual carne: ")).upper()
+    tipo_de_carne = int(input("Qual carne: "))
     quantidade_de_carne = float(input("Quantidade de carne: "))
-    modo_de_pagamento = str(input("Pagar no cartao? (S/N): ")).upper()
+    modo_de_pagamento = str(input("Pagar no cartao? (s/n): "))
     
+    total_da_compra = 0.0
     if tipo_de_carne == 1:
         if quantidade_de_carne <= 5:
             total_da_compra = quantidade_de_carne * 4.90
@@ -25,19 +26,19 @@ def main():
         else:
             total_da_compra = quantidade_de_carne * 7.80
 
-# Cupom
-    if tipo_de_carne == 1:
-        print(f"Tipo de carne:          {tipo_de_carne}")
-        print(f"Quantidade de carne:    {quantidade_de_carne}")
-        print(f"Preco total:            {}")
-        if modo_de_pagamento == "S":
-            print("Tipo de pagamento:  Cartão")
-        else:
-            print("Tipo de pagamento:   Dinheiro")
-        print(f"Valor do desconto:      {}")
-        print(f"Valor a pagar:          {}")
+    desconto = 0.0
+    if modo_de_pagamento == 's'
+        desconto = total_da_compra * 0.05
 
-def calculo_desconto(valor):
-    return valor * (5/100)
+    valor_total_da_compra = total_da_compra - desconto
+
+    print("--- CUPOM FISCAL ---")
+    print(f'Tipo de carne: {tipo_de_carne}')
+    print(f'Quantidade_de_carne: {quantidade_de_carne}')
+    print(f'Modo de pagamento: {modo_de_pagamento}')
+    print(f'Desconto = {desconto}')
+    print('#####################')
+    print(f'Valor Total: {valor_total_da_compra}')
+
 
 main()
