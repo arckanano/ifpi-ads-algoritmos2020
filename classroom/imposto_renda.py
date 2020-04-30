bfa3281a4f0572d5eb8946bcf6c960547bdf22b7
Guilherme Daniel <gui.dani13@gmail.com>
2020-04-30 11:35:16 -0300

    atividade cálculo imposto de renda

---
+++ classroom/imposto_renda.py
@@ -0,0 +1,43 @@
+# App calculo IR vigente e corrigida
+def main():
+    '''
+                                Base de cálculo (R$)
+    |-----------------------------------------------------------------------|
+    | VIGENTE                   | CORRIGIDA                 | ALÍQUOTA (%)  |
+    ------------------------------------------------------------------------|
+    |até 1.903,98               | até 3.881,65              |   ISENTO      |
+    |de 1.903,99 até 2.826,65   | de 3.881,66 até 5.714,11  |   7,5         |
+    |de 2.826,66 até 3.751,05   | de 5.714,12 até 7.654,67  |   15          |
+    |de 3.751,06 até 4.664,68   | de 7.654,68 até 9.564,42  |   22,5        |
+    |acima de 4.664,68          | acima de 9.564,42         |   27,5        |
+    |-----------------------------------------------------------------------|
+    '''
+
+    salario = float(input("Inserir salário: "))
+    # tabela_ir_corrigida(salario)
+
+    ir_faixa_1 = (salario - 1903.99) * 0.075
+    ir_faixa_2 = ir_faixa_1 + (3751.05 - 2826.66) * 0.15
+    ir_faixa_3 = (4664.68 - 3751.06) * 0.225
+    ir_faixa_4 = (salario - 4664.69) * 0.275
+    
+    tabela_ir_vigente(salario, ir_faixa_1, ir_faixa_2, ir_faixa_3, ir_faixa_4)
+
+def tabela_ir_vigente(salario, ir_faixa_1, ir_faixa_2, ir_faixa_3, ir_faixa_4):
+    valor_a_pagar = 0
+    if salario <= 1903.98:
+        print('Isento')
+    elif salario >= 1903.99 and salario <= 2826.65:
+        valor_a_pagar = ir_faixa_1
+        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+    elif salario >= 2826.66 and salario <= 3751.05:
+        valor_a_pagar = ir_faixa_1 + ir_faixa_2
+        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+    elif salario >= 3751.06 and salario <= 4664.68:
+        valor_a_pagar = ir_faixa_1 + ir_faixa_2 + ir_faixa_3
+        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+    else:
+        valor_a_pagar = ir_faixa_1 + ir_faixa_2 + ir_faixa_3 + ir_faixa_4
+        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+
+main()
