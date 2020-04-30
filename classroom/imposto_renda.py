43b1f33fa26883fbdd81e07886641489175e2644
Guilherme Daniel <gui.dani13@gmail.com>
2020-04-30 13:29:30 -0300

    finalização

--- classroom/imposto_renda.py
+++ classroom/imposto_renda.py
@@ -14,41 +14,57 @@ def main():
     '''
 
     salario = float(input("Inserir salário: "))
+    tabela_ir_vigente(salario)
+    tabela_ir_corrigida(salario)
 
+def tabela_ir_vigente(salario):
     # Valor maximo pago por cada faixa
     max_faixa_2 = (2826.65 - 1903.99) * 0.075
     max_faixa_3 = (3751.05 - 2826.66) * 0.15
     max_faixa_4 = (4664.68 - 3751.06) * 0.225
-    # max_faixa_4 = (salario - 4664.69) * 0.275
-
 
     ir_faixa_2 = (salario - 1903.98) * 0.075
     ir_faixa_3 = ((salario - 2826.66) * 0.15) + max_faixa_2
     ir_faixa_4 = ((salario - 3751.06) * 0.225) + max_faixa_3 + max_faixa_2
     ir_faixa_5 = ((salario - 4664.69) * 0.275) + max_faixa_4 + max_faixa_3 + max_faixa_2
-    
-    print('faixa 2', ir_faixa_2)
-    print('faixa 3', ir_faixa_3)
-    print('faixa 4', ir_faixa_4)
-    print('faixa 4', ir_faixa_5)
-    
-    # tabela_ir_vigente(salario, ir_faixa_2, ir_faixa_3, ir_faixa_4)
 
-def tabela_ir_vigente(salario, ir_faixa_2, ir_faixa_3, ir_faixa_4):
     valor_a_pagar = 0
+
     if salario <= 1903.98:
         print('Isento')
     elif salario >= 1903.99 and salario <= 2826.65:
-        valor_a_pagar = ir_faixa_1
-        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+        print(f'Imposto a pagar pela tabela vigente: R${ir_faixa_2:.2f}')
     elif salario >= 2826.66 and salario <= 3751.05:
-        valor_a_pagar = ir_faixa_1 + ir_faixa_2
-        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+        print(f'Imposto a pagar pela tabela vigente: R${ir_faixa_3:.2f}')
     elif salario >= 3751.06 and salario <= 4664.68:
-        valor_a_pagar = ir_faixa_1 + ir_faixa_2 + ir_faixa_3
-        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+        print(f'Imposto a pagar pela tabela vigente: R${ir_faixa_4:.2f}')
     else:
-        valor_a_pagar = ir_faixa_1 + ir_faixa_2 + ir_faixa_3 + ir_faixa_4
-        print(f'Valor a pagar de ir R${valor_a_pagar:.2f}')
+        print(f'Imposto a pagar pela tabela vigente: R${ir_faixa_5:.2f}')
+
+
+def tabela_ir_corrigida(salario):
+    # Valor maximo pago por cada faixa
+    max_faixa_2 = (5714.11 - 1903.99) * 0.075
+    max_faixa_3 = (7654.67 - 5714.12) * 0.15
+    max_faixa_4 = (9564.42 - 7654.68) * 0.225
+
+    ir_faixa_2 = (salario - 1903.98) * 0.075
+    ir_faixa_3 = ((salario - 5714.12) * 0.15) + max_faixa_2
+    ir_faixa_4 = ((salario - 7654.68) * 0.225) + max_faixa_3 + max_faixa_2
+    ir_faixa_5 = ((salario - 9564.42) * 0.275) + max_faixa_4 + max_faixa_3 + max_faixa_2
+
+    valor_a_pagar = 0
+
+    if salario <= 3881.65:
+        print('Imposto a pagar pela tabela corrigida: Isento')
+    elif salario >= 3881.66 and salario <= 5714.11:
+        print(f'Imposto a pagar pela tabela corrigida : R${ir_faixa_2:.2f}')
+    elif salario >= 5714.12 and salario <= 7654.67:
+        print(f'Imposto a pagar pela tabela corrigida: R${ir_faixa_3:.2f}')
+    elif salario >= 7654.68 and salario <= 9564.42:
+        print(f'Imposto a pagar pela tabela corrigida: R${ir_faixa_4:.2f}')
+    else:
+        print(f'Imposto a pagar pela tabela corrigida: R${ir_faixa_5:.2f}')
+
 
 main()
