a97278ed945fde0d46ffbb76d478a89f7c53ce90
Guilherme Daniel <gui.dani13@gmail.com>
2020-08-18 10:18:00 -0300

    adicionado questão 06

--- Fabio03_while/q06-tabuada.py
+++ Fabio03_while/q06-tabuada.py
@@ -1,12 +1,51 @@
 def main():
 
-    i = 1
-    contador = 1
-    while contador <= 10:
-        for i in range(11):
-            print(f'{contador} + {i} = {contador + i}')
-            i += 1
-        print('-' * 10)
-        contador += 1
+    # Exibir tabuada dos 10 primeiros números
+
+    def soma(n=10):
+        contador = 1
+        while contador <= n:
+            s = contador + n
+            print(f'{s} + {contador} = {s}')
+            contador += 1
+    
+
+    def sub(n=10):
+        contador = 1
+        while contador <= n:
+            s = n - contador
+            print(f'{n} - {contador} = {s}')
+            contador += 1
+
+
+    def multiplicar(n=10):
+        contador = 1
+        while contador <= n:
+            m = contador * n
+            print(f'{n} * {contador} = {m}')
+            contador += 1
+
+
+    def div(n=10):
+        contador = 1
+        while contador <= n:
+            d = n // contador
+            print(f'{n} / {contador} = {d}')
+            contador += 1
+
+
+    def resultados():
+        print('--- Tabuada de soma ---')
+        soma()
+        print('--- Tabuada de subtração ---')
+        sub()
+        print('--- Tabuada de multiplicação ---')
+        multiplicar()
+        print('--- Tabuada de divisão ---')
+        div()
+
+
+    resultados()
+
 
 main()
