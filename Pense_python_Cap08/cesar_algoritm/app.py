d73c9173037c1387b4a02b2d5d26be7d3d55486d
guilherme-linux <gui.dani13@gmail.com>
2020-09-04 15:12:29 -0300

    retirado do codigo conversoes em cada chamada de funçao. Agradeço ao Costa pela dica

--- Pense_python_Cap08/cesar_algoritm/app.py
+++ Pense_python_Cap08/cesar_algoritm/app.py
@@ -20,46 +20,46 @@ def confere(palavra, passo):
     for letra in palavra:
         if (ord(letra) == 32):
             print(letra, end='')
-        elif numero(letra) == True:
+        elif numero(ord(letra)) == True:
             print(letra, end='')
-        elif letra_minuscula(letra) == True:
-            virar_minuscula(letra, passo)
-        elif letra_maiuscula(letra) == True:
-            virar_maiuscula(letra, passo)
+        elif letra_minuscula(ord(letra)) == True:
+            virar_minuscula(ord(letra), passo)
+        elif letra_maiuscula(ord(letra)) == True:
+            virar_maiuscula(ord(letra)  , passo)
 
 
 def virar_minuscula(letra, passo):
-    if (ord(letra) + passo) > 122:
-        a = (ord(letra) + passo) - 26
+    if (letra + passo) > 122:
+        a = (letra + passo) - 26
         print(chr(a), end='')
     else:
-        print(chr(ord(letra) + passo), end='')
+        print(chr(letra + passo), end='')
 
 
 def virar_maiuscula(letra, passo):
-    if (ord(letra) + passo) > 90:
-        a = (ord(letra) + passo) - 26
-        print(chr(a), end=' ')
+    if (letra + passo) > 90:
+        a = (letra + passo) - 26
+        print(chr(a), end='')
     else:
-        print(chr(ord(letra) + passo), end=' ')
+        print(chr(letra + passo), end='')
 
 
 def letra_minuscula(letra):
-    if (ord(letra) >= 97 and ord(letra) <= 122):
+    if (letra >= 97 and letra <= 122):
         return True
     else:
         return False
 
 
 def letra_maiuscula(letra):
-    if (ord(letra) >= 65 and ord(letra) <= 90):
+    if (letra >= 65 and letra <= 90):
         return True
     else:
         return False
 
 
 def numero(letra):
-    if (ord(letra) >= 48 and ord(letra) <= 57):
+    if (letra >= 48 and letra <= 57):
         return True
     else:
         return False
