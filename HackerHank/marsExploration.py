71f6cb70c83b141d19679c4d3e23b4702f987cd2
Guilherme Daniel <gui.dani13@gmail.com>
2020-09-13 15:53:59 -0300

    marsExploration correção com a solução enviada ao HackerHank

--- HackerHank/marsExploration.py
+++ HackerHank/marsExploration.py
@@ -5,11 +5,17 @@ def main():
 
 
 def marsExploration(s):
-    c = 0
+    palavra = 'SOS'
+    sub = ''
+    e = 0
     for i in range(len(s)):
-        if s[i] not in 'SO':
-            c += 1
-    return c
+        sub += s[i]
+        if len(sub) % 3 == 0:
+            for j in range(len(sub)):
+                if sub[j] != palavra[j]:
+                    e += 1
+            sub = ''
+    return e
 
 
 main()
